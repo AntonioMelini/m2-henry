5f6f46d2ec416a572b7a81d1d68c847d9013106c
AntonioMelini <antoniomelini@hotmail.com>
2022-04-26 09:21:21 -0300

    homeworl-09

--- 09-React-Routing/homework/src/components/Nav.jsx
+++ 09-React-Routing/homework/src/components/Nav.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import Logo from '../img/logoHenry.png'
 import SearchBar from './SearchBar.jsx';
 import './Nav.css';
+import {Link} from "react-router-dom"
+ 
 
 
 function Nav({onSearch}) {
   return (
-    <nav className="navbar navbar-dark bg-dark">
+    <>
+        <nav className="navbar navbar-dark bg-dark">
         <span className="navbar-brand">
           <img id="logoHenry" src={Logo} width="30" height="30" className="d-inline-block align-top" alt="" />
           Henry - Weather App
@@ -14,7 +17,20 @@ function Nav({onSearch}) {
         <SearchBar
           onSearch={onSearch}
         />
+       
+         
+        
     </nav>
+    <div className="link">
+      <Link to="/">
+        Cards
+      </Link>
+      <Link to="/about">
+        About
+      </Link>
+    </div>
+    </>
+
   );
 };
 
